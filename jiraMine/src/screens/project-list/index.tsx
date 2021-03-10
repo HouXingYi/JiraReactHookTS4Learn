97a494edb0689920afff77c6cf9d635b75ab00fe
houxingyi <1026043046@qq.com>
2021-03-10 17:39:27 +0800

    7 - 1 看完

--- jiraMine/src/screens/project-list/index.tsx
+++ jiraMine/src/screens/project-list/index.tsx
@@ -2,11 +2,14 @@ import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import { useState } from "react"
 // import * as qs from 'qs'
-import { useDebounce } from 'utils/index'
+import { useDebounce, useDocumentTitle } from 'utils/index'
 import styled from "@emotion/styled";
 import { ErrorBox } from "components/lib";
 import { useProjects } from "utils/project"
 import { useUsers } from "utils/user";
+
+// import { Helmet } from 'react-helmet'
+
 
 export const ProjectListScreen = () => {
 
@@ -22,8 +25,15 @@ export const ProjectListScreen = () => {
   // 请求用户列表
   const { data: users } = useUsers();
 
+  // 标题
+  useDocumentTitle('项目列表')
+
   return (
     <Container>
+      {/* <Helmet>
+        <title>项目列表</title>
+      </Helmet> */}
+      <h1>项目列表</h1>
       <SearchPanel users={users || []} param={param} setParam={setParam}></SearchPanel>
       <ErrorBox error={error} />
       <List loading={isLoading} users={users || []} dataSource={list || []}></List>
--- jiraMine/src/unauthenticated-app/index.tsx
+++ jiraMine/src/unauthenticated-app/index.tsx
@@ -7,13 +7,25 @@ import styled from "@emotion/styled";
 import logo from "assets/logo.svg"
 import left from "assets/left.svg"
 import right from "assets/right.svg"
+import { useDocumentTitle } from "utils";
+
+// import { Helmet } from 'react-helmet'
+
+
 
 
 export const UnauthenticatedApp = () => {
     const [isRegister, setIsRegister] = useState(false)
     const [error, setError] = useState<Error | null>(null)
+
+    // 标题
+    useDocumentTitle('请登录注册以继续')
+
     return (
         <Container> 
+            {/* <Helmet>
+                <title>请登录或注册以继续</title>
+            </Helmet> */}
             <Header></Header>
             <Background></Background>
             <ShadowCard>
