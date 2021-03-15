c627c2677c2bcf7707d1f7cf44483b1a92af93f5
houxingyi <1026043046@qq.com>
2021-03-15 17:10:46 +0800

    s

--- react-ts-hook-jira-redux-toolkit/src/store/auth.slice.ts
+++ react-ts-hook-jira-redux-toolkit/src/store/auth.slice.ts
@@ -28,9 +28,12 @@ export const selectUser = (state: RootState) => state.auth.user;
 
 export const login = (form: AuthForm) => (dispatch: AppDispatch) =>
   auth.login(form).then((user) => dispatch(setUser(user)));
+
 export const register = (form: AuthForm) => (dispatch: AppDispatch) =>
   auth.register(form).then((user) => dispatch(setUser(user)));
+
 export const logout = () => (dispatch: AppDispatch) =>
   auth.logout().then(() => dispatch(setUser(null)));
+  
 export const bootstrap = () => (dispatch: AppDispatch) =>
   bootstrapUser().then((user) => dispatch(setUser(user)));
