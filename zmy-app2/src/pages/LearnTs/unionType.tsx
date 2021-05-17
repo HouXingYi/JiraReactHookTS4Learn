5a18ebb05fb4f5812f8d2ed6265333494cd106ca
houxingyi <1026043046@qq.com>
2021-05-17 23:00:47 +0800

    s

--- zmy-app2/src/pages/LearnTs/unionType.tsx
+++ zmy-app2/src/pages/LearnTs/unionType.tsx
@@ -58,8 +58,63 @@ function EnumTrigger() {
 // 4-6 函数泛型
 function typeParameter() {
     
+    // 泛型
+    function join<ABC>(first: ABC, second: ABC) {
+        return `${first}${second}`
+    };
+
+    function join2<T, P>(first: T, second: P) {
+        return `${first}${second}`
+    };
+    
+    function map<T>(params: T[]) {
+        return params;
+    };
+
+    map<number>([111, 222])
+
+    join<string>('1', '1');
+
+    join2<string, number>('1', 1);
+    // 不传类型也可以，会有类型推断
+    join2(1, '1');
+
 }
 
+// 4-7 类中的泛型以及泛型类型
+function classtypeParameter() {
+
+    interface Item {
+        name: string;
+    }
+    
+    class DataManager<T extends Item> {
+        constructor(private data: T[]) {
+        }
+        getItem(index: number): string {
+            return this.data[index].name;
+        }
+    }
+
+    let data = new DataManager<Item>([{
+        name: '222'
+    }]);
+
+    data.getItem(0);
+
+    class DataManager2<T extends number | string> {
+        constructor(private data: T[]) {
+        }
+        getItem(index: number): T {
+            return this.data[index];
+        }
+    }
+
+    let data2 = new DataManager2(['2'])
+    console.log('data2', data2)
+
+
+}
 
 
 function demo4() {
@@ -68,8 +123,9 @@ function demo4() {
             <div>4-4 联合类型和类型保护</div>
             <Divider>分割线</Divider>
             <Button type="primary" onClick={trigger}>点击我</Button> <br/>
-            <Button type="primary" onClick={EnumTrigger}>点击我EnumTrigger</Button>
-            <Button type="primary" onClick={typeParameter}>泛型</Button>
+            <Button type="primary" onClick={EnumTrigger}>点击我EnumTrigger</Button> <br/>
+            <Button type="primary" onClick={typeParameter}>泛型</Button> <br/>
+            <Button type="primary" onClick={classtypeParameter}>类中的泛型</Button>
         </div>
     )
 }
